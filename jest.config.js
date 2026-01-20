df068444e9046d8d4818517655063e08cf14c3a7
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-20 23:15:44 +0100

    feat: Add globals configuration for ts-jest in Jest setup

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,11 @@ const config = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.jest.json',
+    },
+  },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
