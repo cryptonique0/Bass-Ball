103dbac6ddd272f0b2820b6d6a3aede77f3bb232
cryptonique0 <abdulganiyu838@gmail.com>
2026-01-20 03:01:03 +0100

    Remove PHASE documentation and project completion reports

---
+++ public/G II/eslint.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+/** @type {import('eslint').ESLint.ConfigData[]} */
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next
+  globalIgnores(
+    ".next/**",
+    "out/**",
+    "build/**",
+    "dist/**",
+    "node_modules/**",
+    "next-env.d.ts",
+  ),
+]);
+
+export default eslintConfig;
